c10c2e895189f4571dfcc14bfa47ea7ffd740fef
Hawon-Lee <vmfhrm2@korea.ac.kr>
2025-07-21 17:10:19 +0900

    Add dataloader & dacon score calculator

---
+++ src/dataloader.py
@@ -0,0 +1,69 @@
+import json
+import os
+import torch
+from typing import Union
+from pathlib import Path
+from torch.utils.data import Dataset, DataLoader
+
+
+class CustomDataset(Dataset):
+    def __init__(self, data_paths: list[str]):
+        self.data_paths = data_paths
+
+    def __len__(self):
+        return len(self.data_paths)
+
+    def __getitem__(self, index):
+        return torch.load(self.data_paths[index], weights_only=False)
+
+
+class MasterDataLoader:
+    def __init__(
+        self,
+        data_dir: str,
+        data_split_file: str,
+        batch_size: int = 32,
+        num_workers: int = 12,
+    ):
+        self.data_dir = data_dir
+        self.batch_size = batch_size
+        self.num_workers = num_workers
+
+        with open(data_split_file, "r") as fp:
+            data_split = json.load(fp)
+
+        self.tr_keys: list = data_split["train"]
+        self.vl_keys: list = data_split["validation"]
+
+        self.tr_paths: list = [os.path.join(data_dir, k) + ".pt" for k in self.tr_keys]
+        self.vl_paths: list = [os.path.join(data_dir, k) + ".pt" for k in self.vl_keys]
+
+    def tr_dataloader(self, **args):
+        tr_dataset = CustomDataset(self.tr_paths, **args)
+        tr_dataloader = DataLoader(
+            tr_dataset, batch_size=self.batch_size, num_workers=self.num_workers, **args
+        )
+        print(f"Successfully loaded {len(tr_dataset)} train data")
+
+        return tr_dataloader
+
+    def vl_dataloader(self, **args):
+        vl_dataset = CustomDataset(self.vl_paths, **args)
+        vl_dataloader = DataLoader(
+            vl_dataset, batch_size=self.batch_size, num_workers=self.num_workers, **args
+        )
+        print(f"Successfully loaded {len(vl_dataset)} validation data")
+
+        return vl_dataloader
+
+
+if __name__ == "__main__":
+    mdl = MasterDataLoader(
+        data_dir="../data/input",
+        data_split_file="../data/tr_vl_split_250717.json",
+        batch_size=32,
+        num_workers=12,
+    )
+
+    tr_dataloader = mdl.tr_dataloader(shuffle=True)
+    vl_dataloader = mdl.vl_dataloader(shuffle=False)
--- src/evaluate.py
+++ src/evaluate.py
@@ -1,9 +1,40 @@
 import numpy as np
 import pandas as pd
 import torch
+from typing import Union
 from datetime import datetime
+from scipy.stats import pearsonr
+from sklearn.metrics import root_mean_squared_error
 
-# def calc_score() -> 점수 계산하는 metric.
+
+def calc_dacon_score(
+    pred: Union[np.ndarray, torch.Tensor],
+    target: Union[np.ndarray, torch.Tensor]
+) -> float:
+
+    if isinstance(pred, torch.Tensor):
+        pred = pred.numpy()
+    if isinstance(target, torch.Tensor):
+        target = target.numpy()
+
+    # ----------- 경진대회 평가 지표 계산 -----------
+    # 컴포넌트 A: IC50(nM) 단위의 Normalized RMSE
+    rmse_ic50 = root_mean_squared_error(target, pred)
+    range_ic50 = np.max(target) - np.min(pred)
+    norm_rmse = rmse_ic50 / range_ic50 if range_ic50 != 0 else 0
+    component_A = norm_rmse
+
+    # 컴포넌트 B: pIC50 기준 Pearson R^2
+    pearson_corr, _ = pearsonr(target, pred)
+    component_B = pearson_corr**2
+    # 최종 점수 계산
+    score = 0.4 * (1 - min(component_A, 1)) + 0.6 * component_B
+    # print("\n--- 경진대회 평가 결과 ---")
+    # print(f"Normalized RMSE (A): {component_A:.4f}")
+    # print(f"Pearson R^2 (B): {component_B:.4f}")
+    # print(f"최종 점수: {score:.4f}")
+
+    return score
 
 
 def submit(
@@ -16,12 +47,14 @@ def submit(
     if isinstance(pred, torch.Tensor):
         pred = torch.from_numpy(pred)
         print("pred shape:", pred.shape)
-        
+
     if len(pred.shape) == 2:
         pred = pred.ravel()
-        
+
     if convert_pIC_to_IC:
-        print("입력된 값을 PIC50에서 IC50으로 변환합니다. 원치 않을 경우 convert_pIC_to_IC를 False로 변경하세요")
+        print(
+            "입력된 값을 PIC50에서 IC50으로 변환합니다. 원치 않을 경우 convert_pIC_to_IC를 False로 변경하세요"
+        )
         pred = 10 ** (9 - pred) / 10e9
 
     submission = pd.read_csv(source_csv)
@@ -39,6 +72,11 @@ def submit(
 
     return submission
 
+
 if __name__ == "__main__":
-    test_array = np.random.randn(1, 127)
-    submit(test_array)
+    # 테스트 코드
+    pred_array = np.random.randn(1, 127)
+    target_array = np.random.randn(1, 127)
+    
+    calc_dacon_score(pred_array, target_array)
+    submit(pred_array)
